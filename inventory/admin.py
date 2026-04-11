a45331c25c1cd697531713353f19f354816bc6ba
Vaibhav Shukla <keep.vaibin@gmail.com>
2026-04-11 14:13:39 -0700

    Phases 1-11: Backend models, serializers, views, URLs, signals, TCG import

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Item, ItemImage, WantedCard, WantedCardImage, PickupSlot, PokeshopSettings, PickupTimeslot
+from .models import (
+    Item, ItemImage, WantedCard, WantedCardImage, PickupSlot,
+    PokeshopSettings, PickupTimeslot, Category, SubCategory,
+    PromoBanner, HomepageSection,
+)
 
 admin.site.register(Item)
 admin.site.register(ItemImage)
@@ -8,3 +12,7 @@ admin.site.register(WantedCardImage)
 admin.site.register(PickupSlot)
 admin.site.register(PokeshopSettings)
 admin.site.register(PickupTimeslot)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(PromoBanner)
+admin.site.register(HomepageSection)
