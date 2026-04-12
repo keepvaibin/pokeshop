1ac9add69b82005c9a156b902f26eb0c7568e9da
Vaibhav Shukla <keep.vaibin@gmail.com>
2026-04-12 01:25:36 -0700

    V25.1 Admin Settings Discord UI Patch

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('google/', views.GoogleAuthView.as_view(), name='google_auth'),
+    path('discord/login/', views.DiscordOAuthInitiateView.as_view(), name='discord_oauth_login'),
     path('discord/initiate/', views.DiscordOAuthInitiateView.as_view(), name='discord_oauth_initiate'),
     path('discord/callback/', views.DiscordOAuthCallbackView.as_view(), name='discord_oauth_callback'),
     path('user/', views.CurrentUserView.as_view(), name='current_user'),
